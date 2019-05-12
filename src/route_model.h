9a4129dbd91ae63201e647fb2bd7996262896dfd
dbecad <dbecadgit@gmail.com>
2019-05-11 23:19:20 -0700

    05 Adding Node>Road reverse map

--- src/route_model.h
+++ src/route_model.h
@@ -5,19 +5,26 @@
 #include <unordered_map>
 #include "model.h"
 #include <iostream>
+#include <vector>
 
 class RouteModel : public Model {
 
   public:
+
+    //A* Enabled Nodes
     class Node : public Model::Node {
       public:
-        // Add public Node variables and methods here.
+        //previous Node in chain
         RouteModel::Node* parent = nullptr;
+        //Heuristics distance and cost from start
         float h_value = std::numeric_limits<float>::max();
         float g_value = 0.f;
+        //Was node already visited
         bool visited = false;
+        //List of contiguous nodes
         std::vector<RouteModel::Node *> neighbors;
         
+        //Node constructor extending OSM nodes
         Node(){}
         Node(int idx, RouteModel* search_model, Model::Node node) : 
             Model::Node(node), 
@@ -28,20 +35,30 @@ class RouteModel : public Model {
         float distance(const Model::Node &otherNode) const;
       
       private:
-        // Add private Node variables and methods here.
+        //Node index in list & parent reference
         int index;
         RouteModel* parent_model = nullptr;
     };
     
-    // Add public RouteModel variables and methods here.
-    RouteModel(const std::vector<std::byte> &xml);  
-    std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
+    //Route model constructor
+    RouteModel(const std::vector<std::byte> &xml);
 
-    //getter for the private nodes vector
+    //Reverse map getter (used for test)
+    auto &GetNodeToRoadMap() { return node_to_road; }
+
+    //Path that is found by the A* search.  
+    std::vector<Node> path; 
+
+    //Getter for the private nodes vector
     std::vector<Node> &SNodes() { return m_Nodes; }
 
   private:
-    // Add private RouteModel variables and methods here.
+    //Building of the reverse map
+    void CreateNodeToRoadHashmap(void);
+
+    //List of A* enabled Nodes
     std::vector<RouteModel::Node> m_Nodes;
 
+    //Reverse map of NodeID -> Road to help the graph search
+    std::unordered_map <int, std::vector<const Model::Road *>> node_to_road;
 };
