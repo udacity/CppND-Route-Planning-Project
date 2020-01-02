da6606c8bfac339ddd33f36c2260f8589bde4fe2
ahmedamsoliman-1 <ahmed-soliman@hcl.com>
2020-01-02 21:15:27 +0400

    pushiiii

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -1,7 +1,9 @@
 #include "route_planner.h"
 #include <algorithm>
 
-RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
+RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y)
+    : m_Model(model) 
+{
     // Convert inputs to percentage:
     start_x *= 0.01;
     start_y *= 0.01;
