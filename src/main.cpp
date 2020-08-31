363690df17b8fdc4baefe9b9d2417af2d5fd555a
masif <mohammad.asif@iav.de>
2020-08-31 17:49:04 +0200

    mod(main.cpp): user input for start and end values

--- src/main.cpp
+++ src/main.cpp
@@ -55,12 +55,17 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x, start_y, end_x, end_y;
+    std::cout << "Enter the values `start_x`, `start_y`, `end_x`, and `end_y`" << std::endl;
+    std::cin >> start_x >> start_y >> end_x >> end_y ;
+    std::cout << "start_x: " << start_x << " start_y: " << start_y << " end_x: " << end_x << " end_y: " << end_y << std::endl;
+
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
