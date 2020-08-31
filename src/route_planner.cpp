487c9258777067beeed67571b5c51a28011b691a
masif <mohammad.asif@iav.de>
2020-08-31 17:50:56 +0200

    mod(route_planner.cpp): closest node for start and end cordinates

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -8,6 +8,8 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     end_x *= 0.01;
     end_y *= 0.01;
 
+    *start_node = m_Model.FindClosestNode(start_x, start_y);
+    *end_node = m_Model.FindClosestNode(end_x, end_y);
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
 
