988f463bad59559c782f8293447583ad552539b8
masif <mohammad.asif@iav.de>
2020-08-31 17:54:07 +0200

    mod(route_planner.cpp): implemented the CalculateHValue method (TODO 3)

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -22,7 +22,7 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 // - Node objects have a distance method to determine the distance to another node.
 
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
-
+    return node->distance(*end_node);
 }
 
 
