9f12df37f603e873f8d5d9d987b0cfc1f79da60b
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2024-04-05 10:30:14 -0400

    feat: adding coordinate input to main

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include "route_planner.h"
 
 using namespace std::experimental;
+using std::cin;
+using std::cout;
 
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
 {   
@@ -36,34 +38,41 @@ int main(int argc, const char **argv)
                 osm_data_file = argv[i];
     }
     else {
-        std::cout << "To specify a map file use the following format: " << std::endl;
-        std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;
+        cout << "To specify a map file use the following format: " << std::endl;
+        cout << "Usage: [executable] [-f filename.osm]" << std::endl;
         osm_data_file = "../map.osm";
     }
     
     std::vector<std::byte> osm_data;
  
     if( osm_data.empty() && !osm_data_file.empty() ) {
-        std::cout << "Reading OpenStreetMap data from the following file: " <<  osm_data_file << std::endl;
+        cout << "Reading OpenStreetMap data from the following file: " <<  osm_data_file << std::endl;
         auto data = ReadFile(osm_data_file);
         if( !data )
-            std::cout << "Failed to read." << std::endl;
+            cout << "Failed to read." << std::endl;
         else
             osm_data = std::move(*data);
     }
-    
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
+
+    float start_x, start_y, end_x, end_y;
+    cout << "Provide the start and end position's coordinates for the route.\n";
+    cout << "Start's x coordinate: ";
+    cin >> start_x;
+    cout << "Start's y coordinate: ";
+    cin >> start_y;
+    cout << "End's x coordinate: ";
+    cin >> end_x;
+    cout << "End's y coordinate: ";
+    cin >> end_y;
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
-    std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
+    cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
 
     // Render results of search.
     Render render{model};
