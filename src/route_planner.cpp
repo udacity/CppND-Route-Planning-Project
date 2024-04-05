db4decd8f88cc1f952201c0e02db29b9a9975583
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2024-04-05 11:03:34 -0400

    feat: updating the RoutePlanner's constructor

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -8,9 +8,9 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     end_x *= 0.01;
     end_y *= 0.01;
 
-    // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
-
+    this->start_node = &model.FindClosestNode(start_x, start_x);
+    this->end_node = &model.FindClosestNode(end_x, end_y);
 }
 
 
